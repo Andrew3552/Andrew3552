b4d58a2049c6df695f9adca1a3b5cc0b55069e95
Svitlana Pedchenko <tulipromashka@gmail.com>
2023-08-03 10:33:44 +0300

    sms

--- src/js/app.js
+++ src/js/app.js
@@ -15,27 +15,4 @@ $(document).ready(function () {
 
     console.log(e.target);
   });
-
-
-import BaseHelpers from './helpers/BaseHelpers.js';
-
-
-BaseHelpers.checkWebpSupport();
-
-
-const burgerOpenBtn = document.getElementById('open');
-const burgerClosedBtn = document.getElementById('closed');
-const burgerMenuList = document.getElementById('menu');
-
-burgerOpenBtn.addEventListener('click', () => {
-  burgerOpenBtn.classList.add('hide');
-  burgerClosedBtn.classList.remove('hide');
-  burgerMenuList.classList.remove('hide');
-});
-
-burgerClosedBtn.addEventListener('click', () => {
-  burgerOpenBtn.classList.remove('hide');
-  burgerClosedBtn.classList.add('hide');
-  burgerMenuList.classList.add('hide');
-
-});
+})
